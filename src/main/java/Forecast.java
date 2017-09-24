435e50bbf6cd0e32ef148b8dd6bddc0b545bc2c7
arturkaldma <arturka.kaldma@gmail.com>
2017-09-25 00:30:18 +0300

    first commit

---
+++ src/main/java/Forecast.java
@@ -0,0 +1,5 @@
+/**
+ * Created by artur on 24.09.2017.
+ */
+public class Forecast {
+}
---
+++ src/test/java/ee/ttu/weather/test/WeatherTest.java
@@ -0,0 +1,43 @@
+package ee.ttu.weather.test;
+
+/**
+ * Created by artur on 25.09.2017.
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WeatherTest {
+
+    @Test
+    public void getWeatherAtNow(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void getWeatherThreeDays(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void getCurrrentTemperature(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void getWindSpeed(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void getLocation(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testConnectionExist() {
+        fail("Not yet implemented");
+    }
+}
+
